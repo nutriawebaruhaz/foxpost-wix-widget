576ba1ad918fd51589fcb8d42e69747ba3b91e8c
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-21 09:06:00 +0200

    Register FOXPOST setup dashboard page

--- wix-app/src/extensions.ts
+++ wix-app/src/extensions.ts
@@ -1,7 +1,9 @@
 import { app } from '@wix/astro/builders';
 import foxpostCheckout from './extensions/site/plugins/foxpost-checkout/foxpost-checkout.extension';
 import foxpostShippingRates from './extensions/backend/service-plugins/foxpost-shipping-rates/foxpost-shipping-rates.extension';
+import foxpostSetup from './extensions/dashboard/pages/foxpost-setup/foxpost-setup.extension';
 
 export default app()
   .use(foxpostCheckout)
-  .use(foxpostShippingRates);
+  .use(foxpostShippingRates)
+  .use(foxpostSetup);
