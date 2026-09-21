ca07d084216b0312e21d333fef6395575a71e7c8
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-21 09:08:08 +0200

    Configure FOXPOST checkout validation service plugin

---
+++ wix-app/src/extensions/backend/service-plugins/foxpost-validation/foxpost-validation.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.ecomValidations({
+  id: 'e6621683-ab81-419a-87e7-a226ec9857ba',
+  name: 'foxpost-validation',
+  validateInCart: false,
+  source: './extensions/backend/service-plugins/foxpost-validation/foxpost-validation.ts',
+});
