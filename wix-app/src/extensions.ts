1f025593cade6dc2615d4c97dc2aa139e9fa2f37
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-21 09:08:06 +0200

    Register FOXPOST checkout validation

--- wix-app/src/extensions.ts
+++ wix-app/src/extensions.ts
@@ -1,9 +1,11 @@
 import { app } from '@wix/astro/builders';
 import foxpostCheckout from './extensions/site/plugins/foxpost-checkout/foxpost-checkout.extension';
 import foxpostShippingRates from './extensions/backend/service-plugins/foxpost-shipping-rates/foxpost-shipping-rates.extension';
+import foxpostValidation from './extensions/backend/service-plugins/foxpost-validation/foxpost-validation.extension';
 import foxpostSetup from './extensions/dashboard/pages/foxpost-setup/foxpost-setup.extension';
 
 export default app()
   .use(foxpostCheckout)
   .use(foxpostShippingRates)
+  .use(foxpostValidation)
   .use(foxpostSetup);
