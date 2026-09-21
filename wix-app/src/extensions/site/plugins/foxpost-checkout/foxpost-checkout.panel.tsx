20396fc077af0be4f8ee3f3487a7bc0440b8c935
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-21 11:29:04 +0200

    Add minimal FOXPOST checkout settings panel

---
+++ wix-app/src/extensions/site/plugins/foxpost-checkout/foxpost-checkout.panel.tsx
@@ -0,0 +1,23 @@
+import React, { type FC } from 'react';
+import {
+  SidePanel,
+  Text,
+  WixDesignSystemProvider,
+} from '@wix/design-system';
+import '@wix/design-system/styles.global.css';
+
+const Panel: FC = () => (
+  <WixDesignSystemProvider>
+    <SidePanel width="300" height="100vh">
+      <SidePanel.Content noPadding stretchVertically>
+        <SidePanel.Field>
+          <Text>
+            A FOXPOST checkout plugin beállításai a Nutri-A alkalmazásból kezelhetők.
+          </Text>
+        </SidePanel.Field>
+      </SidePanel.Content>
+    </SidePanel>
+  </WixDesignSystemProvider>
+);
+
+export default Panel;
