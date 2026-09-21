5d9ee278a1b439c8c5bf21277f6d3a56ad135d81
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-21 09:06:02 +0200

    Add FOXPOST setup dashboard page extension

---
+++ wix-app/src/extensions/dashboard/pages/foxpost-setup/foxpost-setup.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '9ae48207-16ce-4759-a5bd-fa829a2302fc',
+  title: 'FOXPOST Checkout',
+  routePath: 'foxpost-checkout',
+  component: './extensions/dashboard/pages/foxpost-setup/foxpost-setup.tsx',
+});
