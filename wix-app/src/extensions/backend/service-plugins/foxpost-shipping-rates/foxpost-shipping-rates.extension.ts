4f859bb8611a33480922874dcebdc08d0980c698
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-22 20:16:32 +0200

    Use Foxpost casing in shipping extension description

--- wix-app/src/extensions/backend/service-plugins/foxpost-shipping-rates/foxpost-shipping-rates.extension.ts
+++ wix-app/src/extensions/backend/service-plugins/foxpost-shipping-rates/foxpost-shipping-rates.extension.ts
@@ -3,7 +3,7 @@ import { extensions } from '@wix/astro/builders';
 export default extensions.ecomShippingRates({
   id: 'e7816e6d-f15e-4236-88db-7ca746ca5497',
   name: 'foxpost-shipping-rates',
-  description: 'FOXPOST pickup-point delivery for Nutri-A checkout.',
+  description: 'Foxpost pickup-point delivery for Nutri-A checkout.',
   fallbackDefinitionMandatory: false,
   source: './extensions/backend/service-plugins/foxpost-shipping-rates/foxpost-shipping-rates.ts',
 });
