ae598305d2460071946ebfa32f9fe3a6d95456e6
nutriawebaruhaz <nutriakontakt@gmail.com>
2026-09-22 12:01:13 +0200

    Use Foxpost brand casing in visible text

--- wix-app/src/extensions/dashboard/pages/foxpost-setup/foxpost-setup.extension.ts
+++ wix-app/src/extensions/dashboard/pages/foxpost-setup/foxpost-setup.extension.ts
@@ -2,7 +2,7 @@ import { extensions } from '@wix/astro/builders';
 
 export default extensions.dashboardPage({
   id: '9ae48207-16ce-4759-a5bd-fa829a2302fc',
-  title: 'FOXPOST Checkout',
+  title: 'Foxpost Checkout',
   routePath: 'foxpost-checkout',
   component: './extensions/dashboard/pages/foxpost-setup/foxpost-setup.tsx',
 });
